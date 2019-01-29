b81a891f2ae3a40580ea279b86e7b46702d07fd1
Spencer Brooks <spencer@databit.io>
2019-01-28 21:06:30 -0700

    enable search bar

---
+++ main.js
@@ -0,0 +1,7 @@
+var placesAutocomplete = places({
+  appId: 'pl4CQ5I7JENG',
+  apiKey: 'cf274226f2fc8ac66bb79f97db34f44f',
+  container: document.querySelector('#address-input')
+});
+
+console.log('test')
